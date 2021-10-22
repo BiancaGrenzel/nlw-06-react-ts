f621963b8e708e60c0566d05da252ae46fb9f425
Bianca Grenzel <bianca.severo@softplan.com.br>
2021-10-22 05:33:38 -0300

    Authentication with Firebase and state management with Context API

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,11 +7,16 @@ import googleIconImg from "../assets/images/google-icon.svg";
 import { Button } from "../components/Button";
 
 import "../styles/auth.scss";
+import { useAuth } from "../hooks/useAuth";
 
 export function Home() {
   const history = useHistory();
+  const { user, signInWithGoogle } = useAuth();
 
-  function navigateToNewRoom() {
+  async function handleCreateRoom() {
+      if(!user){
+          await signInWithGoogle();
+      }
     history.push("/rooms/new");
   }
   return (
@@ -27,7 +32,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button onClick={navigateToNewRoom} className="create-room">
+          <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,14 +1,19 @@
 import { Link } from 'react-router-dom';
 
+// import { useAuth } from '../hooks/useAuth';
+
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
-import googleIconImg from "../assets/images/google-icon.svg";
 
 import { Button } from "../components/Button";
 
 import "../styles/auth.scss";
 
+// import { AuthContext } from '../contexts/AuthContext';
+// import { useContext } from 'react';
+
 export function NewRoom() {
+    // const { user } = useAuth();
   return (
     <div id="page-auth">
       <aside>
